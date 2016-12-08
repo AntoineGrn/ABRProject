437e56c427e55b146d91fbb95336688a820e2898
Laureen LOISON <E136217M@I019V7pc12.ensinfo.sciences.univ-nantes.prive>
2016-12-08 15:23:24 +0100

    ajout classe controller et main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import controleur.Controller;
 
 public class Main {
-	public static void main (String[] args){
+	
+	public static void main (String[] args) throws IOException, URISyntaxException{
+		Controller myController = new Controller();
 	    System.out.println("Hello World");
+	    myController.readFileABR("./exemples.figure1.txt");
 	}
 }
---
+++ src/controleur/Controller.java
@@ -0,0 +1,30 @@
+package controleur;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Controller {
+	
+	public Controller() {
+		
+	}
+	
+	
+	public void readFileABR(String filename) throws IOException, URISyntaxException {
+		URI file = new URI(filename);
+		Path path = Paths.get(file);
+		InputStream in = Files.newInputStream(path, null);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+	    String line = null;
+	    while ((line = reader.readLine()) != null) {
+	        System.out.println(line);
+	    }
+	}
+}
