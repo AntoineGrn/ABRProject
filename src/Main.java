e5d7be034bb05d60f081f22d76342733a11c22ce
AntoineGrn <antoinegrn12@gmail.com>
2016-12-08 16:42:06 +0100

    MAJ des imports

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 
 import controleur.Controller;
 
@@ -15,6 +9,6 @@ public class Main {
 	public static void main (String[] args) throws IOException, URISyntaxException{
 		Controller myController = new Controller();
 	    System.out.println("Hello World");
-	    myController.readFileABR("figure1.txt");
+	    myController.readFileABR("bin/exemples/figure1.txt");
 	}
 }
--- src/controleur/Controller.java
+++ src/controleur/Controller.java
@@ -1,29 +1,20 @@
 package controleur;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import model.ABR;
+
 
 public class Controller {
 	
 	public Controller() {
 		
 	}
-	
-	
+
 	public void readFileABR(String filename) throws IOException, URISyntaxException {
-		BufferedReader br = new BufferedReader(new FileReader("bin/exemples/figure1.txt"));
+		BufferedReader br = new BufferedReader(new FileReader(filename));
 	    String line = null;
 	    while ((line = br.readLine()) != null) {
 	        System.out.println(line);
