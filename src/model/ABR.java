4777528bb789f6a76984294e22b64e5f45c003ce
AntoineGrn <antoinegrn12@gmail.com>
2016-12-08 15:32:10 +0100

    Constructeur de l'ABR

--- src/model/ABR.java
+++ src/model/ABR.java
@@ -1,5 +1,37 @@
 package model;
 
 public class ABR {
+    private int debut;
+    private int fin;
+    private  int[] parcoursSuffixe;
 
+    public ABR(int debut, int fin, int[] parcoursSuffixe) {
+        this.debut = debut;
+        this.fin = fin;
+        this.parcoursSuffixe = parcoursSuffixe;
+    }
+
+    public int getDebut() {
+        return debut;
+    }
+
+    public void setDebut(int debut) {
+        this.debut = debut;
+    }
+
+    public int getFin() {
+        return fin;
+    }
+
+    public void setFin(int fin) {
+        this.fin = fin;
+    }
+
+    public int[] getParcoursSuffixe() {
+        return parcoursSuffixe;
+    }
+
+    public void setParcoursSuffixe(int[] parcoursSuffixe) {
+        this.parcoursSuffixe = parcoursSuffixe;
+    }
 }
