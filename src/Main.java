447adbb65fa7f7712581709e735570f4359d2df9
AntoineGrn <antoinegrn12@gmail.com>
2016-12-13 16:50:05 +0100

    save

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,6 @@ public class Main {
 		Controller myController = new Controller();
 	    System.out.println("Hello World");
 	    myController.readFileABR("bin/exemples/figure1.txt");
-		List<Integer> parcoursSuffixe = new ArrayList<Integer>();
-		parcoursSuffixe.add(11);
-		parcoursSuffixe.add(14);
-		parcoursSuffixe.add(9);
-		//ABR abr = new ABR(9, 22, parcoursSuffixe);
 		//myController.createFileABR(abr);
 		/*ABR abrSAG = new ABR();
 		abrSAG.setRacine(55);
