1bb71e64bba9d86c577cb05b24fddfb8c4fece58
LOISON LAUREEN <lloison@tibco.fr>
2016-12-31 16:48:21 +0100

    ajout et modif de fonctionnalités

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,73 @@ import java.net.URISyntaxException;
 import java.util.*;
 import controleur.Controller;
 import model.ABR;
+import model.TabABR;
 
 public class Main {
 	
 	public static void main (String[] args) throws IOException, URISyntaxException{
 		Controller myController = new Controller();
-	    System.out.println("Hello World");
-	    myController.readFileABR("bin/exemples/figure1.txt");
-		//myController.createFileABR(abr);
-		/*ABR abrSAG = new ABR();
-		abrSAG.setRacine(55);
+	    System.out.println("Bienvenue sur le programme des arbres binaires de recherche \n");
+		/*
+		2.1 Génération, sauvegarde et affichage des TABRs
+		 */
+		System.out.println("------- 2.1 Génération, sauvegarde et affichage des TABRs ----------");
+		System.out.println("------- FICHIER VERS TABR ----------");
+		/* 1. fichier vers TABR */
+		List<TabABR> liste = myController.readFileABR("bin/exemples/figure1.txt");
+		System.out.println("------------------------------------");
+
+		// création d'un ABR manuellement
+		ABR abrSAG = new ABR();
+		abrSAG.setRacine(155);
 		ABR abrSAD2 = new ABR();
-		abrSAD2.setRacine(75);
+		abrSAD2.setRacine(175);
 		ABR abrSAD3 = new ABR();
-		abrSAD3.setRacine(63);
+		abrSAD3.setRacine(161);
 		ABR abrSAD = new ABR();
-		abrSAD.setRacine(62);
+		abrSAD.setRacine(162);
 		abrSAD.setSag(abrSAD3);
 		abrSAD.setSad(abrSAD2);
 
 		ABR arbre = new ABR();
-		arbre.setRacine(60);
+		arbre.setRacine(160);
 		arbre.setSag(abrSAG);
-		arbre.setSad(abrSAD);*/
-		//List<Integer> parcours = myController.getParcoursSuffixe(arbre);
-		//System.out.println(parcours.toString());
+		arbre.setSad(abrSAD);
 
-		//myController.createFileABR(arbre);
+		System.out.println("--------- TABR VERS FICHIER --------");
+		/* 2. TABR vers fichier */
+		myController.createFileABR(arbre);
+		System.out.println("------------------------------------");
+
+		System.out.println("--------- AFFICHER TABR ------------");
+		/* 3. afficher TABR */
+		String abrToString = myController.toStringABR(arbre);
+		System.out.println(abrToString);
+		System.out.println("------------------------------------");
+
+		System.out.println("--------- TABR ALEATOIRE -----------");
+		/* 4. TABR aléatoire */
+		int n = 4;
+		int m = 3;
+		myController.createTABRAleatoire(n, m);
+		System.out.println("------------------------------------");
+
+		System.out.println("--------- VERIFICATION -------------");
+		/* 5. verification TABR */
+		boolean verif = myController.verificationTABR(liste);
+		System.out.println("TABR correct ? " + verif);
+		System.out.println("------------------------------------");
+
+		/*
+		2.2 Manipulation des TABRs
+		 */
+		System.out.println("------- 2.1 Génération, sauvegarde et affichage des TABRs ----------");
+
+		System.out.println("--------- INSERTION ENTIER -------------");
+		/* 1. insertion d'un entier */
+		int nbr = 21;
+		TabABR tabr = myController.insertionEntierIntoTabr(nbr, liste);
+		//myController.addTABRintoFile(tabr);
+		System.out.println("------------------------------------");
 	}
 }
--- src/controleur/Controller.java
+++ src/controleur/Controller.java
@@ -3,6 +3,7 @@ package controleur;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import model.ABR;
 import model.TabABR;
@@ -14,7 +15,7 @@ public class Controller {
 		
 	}
 
-	public void readFileABR(String filename) throws IOException, URISyntaxException {
+	public List<TabABR> readFileABR(String filename) throws IOException, URISyntaxException {
 		BufferedReader br = new BufferedReader(new FileReader(filename));
 	    String line = null;
 	    List<TabABR> listTab = new ArrayList<TabABR>();
@@ -33,6 +34,7 @@ public class Controller {
 	        tabr = this.createTabABR(Integer.parseInt(debut), Integer.parseInt(fin), parcoursSuffixe);
 	        listTab.add(tabr);
 	    }
+	    return listTab;
 	}
 
 	public void createFileABR(ABR tabArbre) {
@@ -51,13 +53,29 @@ public class Controller {
 		}
 	}
 
+	public void addTABRintoFile(TabABR tabr) {
+		File f = new File("bin/exemples/figure1.txt");
+		try
+		{
+			FileWriter fw = new FileWriter (f, true);
+			fw.write (toStringTABR(tabr));
+			fw.write ("\r\n");
+
+			fw.close();
+		}
+		catch (IOException exception)
+		{
+			System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
+		}
+	}
+
 	public TabABR createTabABR(int debut, int fin, List<Integer> parcoursSuffixe) {
 		TabABR tabr = new TabABR();
 		tabr.setFin(fin);
         tabr.setDebut(debut);
         ABR abr = this.createABR(parcoursSuffixe);
         tabr.setArbre(abr);
-        System.out.println(this.toStringTABR(tabr));
+		System.out.println(this.toStringTABR(tabr));
 		return tabr;
 
 	}
@@ -68,13 +86,19 @@ public class Controller {
 		if(parcoursSuffixe.size() != 1) {
 			List<Integer> inferieur = new ArrayList<Integer>();
 			List<Integer> superieur = new ArrayList<Integer>();
+			int compteurInferieur = 0;
+			int compteurSuperieur = 0;
 			for(int index=parcoursSuffixe.size()-1; index >= 0; index--) {
 				if (parcoursSuffixe.get(index) < abr.getRacine()) {
-					inferieur.add(parcoursSuffixe.get(index));
+					inferieur.add(compteurInferieur, parcoursSuffixe.get(index));
+					compteurInferieur++;
 				}else if (parcoursSuffixe.get(index) > abr.getRacine()) {
-					superieur.add(parcoursSuffixe.get(index));
+					superieur.add(compteurSuperieur, parcoursSuffixe.get(index));
+					compteurSuperieur++;
 				}
 			}
+			Collections.reverse(superieur);
+			Collections.reverse(inferieur);
 			if (inferieur.size() > 0) {
 				ABR abrsag = this.createABR(inferieur);
 				abr.setSag(abrsag);
@@ -95,17 +119,17 @@ public class Controller {
 	public List<Integer> getParcoursSuffixe(ABR abr) {
 
 		if (abr.getSag() != null){
-			this.parcoursSuffixe = getParcoursSuffixe(abr.getSag());
+			this.parcoursSuffixe = this.getParcoursSuffixe(abr.getSag());
 		}
 		if (abr.getSad() != null) {
-			this.parcoursSuffixe = getParcoursSuffixe(abr.getSad());
+			this.parcoursSuffixe = this.getParcoursSuffixe(abr.getSad());
 		}
 		this.parcoursSuffixe.add(abr.getRacine());
-
-		return parcoursSuffixe;
+		return this.parcoursSuffixe;
 	}
 
 	public String toStringForWrite(ABR abr) {
+		this.parcoursSuffixe.clear();
 		List<Integer> list = this.getParcoursSuffixe(abr);
 		System.out.println(list.toString().replace("[", "").replace("]", "").replace(", ",":"));
 		int min = list.get(0);
@@ -134,7 +158,79 @@ public class Controller {
 		return result + abr.getRacine();
 	}
 
-	/*public TabABR generateTabABR(int n, int m) {
+	public void createTABRAleatoire(int n, int m) {
+		System.out.println("variable n : " + n + "\nvariable m : " + m);
+	}
 
-	}*/
+	public boolean verificationTABR(List<TabABR> listetabr) {
+		boolean verifContenuABR = true;
+		for (int i = 0; i <= listetabr.size()-1; i++) {
+			if(verifContenuABR) {
+				int debutTmp = listetabr.get(i).getDebut();
+				int finTmp = listetabr.get(i).getFin();
+
+				// verification que le debut de l'intervalle est inférieur à la fin de l'intervalle
+				if(debutTmp>finTmp) verifContenuABR = false;
+
+				// verification que tous les elements de l'abr sont contenus dans l'intervalle du tabr
+				this.parcoursSuffixe.clear();
+				List<Integer> list = this.getParcoursSuffixe(listetabr.get(i).getArbre());
+				int min = list.get(0);
+				int max = list.get(0);
+
+				for(Integer index: list) {
+					if (index < min) min = index;
+					if (index > max) max = index;
+				}
+				if(debutTmp > min || finTmp < max) verifContenuABR = false;
+
+				// verification que tous les intervalles sont dans l'ordre croissant
+				if (i != listetabr.size()-1 && finTmp > listetabr.get(i+1).getDebut()) verifContenuABR = false;
+			}
+		}
+		return verifContenuABR;
+	}
+
+	public TabABR insertionEntierIntoTabr(int nbr, List<TabABR> listeTabr) {
+		boolean intervalleOk = false;
+		TabABR res = null;
+
+		for (TabABR tabr: listeTabr) {
+			if (nbr <= tabr.getFin() && nbr >= tabr.getDebut()){
+				intervalleOk = true;
+				ABR newAbr = this.insertionEntierAbr(nbr, tabr.getArbre());
+				tabr.setArbre(newAbr);
+				System.out.println(this.toStringTABR(tabr));
+				res = tabr;
+			}
+		}
+		if (!intervalleOk) System.out.println("Erreur pendant l'insertion, l'élément que vous voulez ajouter n'appartient à aucun intervalle");
+
+		return res;
+	}
+
+	private ABR insertionEntierAbr(int nbr, ABR abr) {
+		if(nbr < abr.getRacine()) {
+			if(abr.getSag() == null) {
+				ABR nouvelElem = new ABR(nbr, null, null);
+				abr.setSag(nouvelElem);
+				System.out.println("Element ajouté avec succès");
+			}else {
+				insertionEntierAbr(nbr, abr.getSag());
+			}
+		}
+		if(nbr > abr.getRacine()) {
+			if(abr.getSad() == null) {
+				ABR nouvelElem = new ABR(nbr, null, null);
+				abr.setSad(nouvelElem);
+				System.out.println("Element ajouté avec succès");
+			}else {
+				insertionEntierAbr(nbr, abr.getSad());
+			}
+		}
+		if(nbr == abr.getRacine()) {
+			System.out.println("Erreur pendant l'insertion, l'élément que vous voulez ajouter existe déjà");
+		}
+		return abr;
+	}
 }
